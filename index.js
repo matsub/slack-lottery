6750b9ee835f797533b360e61c4ecaeaf790eae9
matsub <matsub.rk@gmail.com>
2019-07-18 11:24:36 +0900

    quit to use equal symbol

--- index.js
+++ index.js
@@ -134,7 +134,6 @@ function slashcommand(feature) {
       res.setHeader("Content-Type", "application/json");
       res.send(message.body);
     } else {
-      console.log(process.env.WEBHOOK_ENDPOINT);
       await fetch(process.env.WEBHOOK_ENDPOINT, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
